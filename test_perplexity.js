fba33f1aabef25b39aa894ca07083b2cd87bd281
Nok <chulayuth@atvantage.co.th>
2017-11-22 00:35:16 +0700

    Implement perplexity calculation using let-to-right method for LDA

---
+++ test_perplexity.js
@@ -0,0 +1,15 @@
+var lda = require('./lib/lda');
+var perplexity_lda = require('./lib/perplexity_lda');
+
+var text = 'Cats are small. Dogs are big. Cats like to chase mice. Dogs like to eat bones.';
+var text_test = 'Cats are small. You are small. Tigers are big, Cats chase mice. Dogs eat mice.';
+var documents = text.match( /[^\.!\?]+[\.!\?]+/g );
+var documents_test = text_test.match( /[^\.!\?]+[\.!\?]+/g );
+
+var result = lda(documents, 2, 5);
+
+console.log(JSON.stringify(result));
+result.printReadableOutput();
+
+var perplexity = perplexity_lda(result.topicModel, documents_test);
+console.log('Perplexity = '+perplexity);
