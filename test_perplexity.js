a58486b7ffdc52a72bf372e6d30c326751d9d482
Nok <nok@omnivirt.com>
2017-12-02 18:52:25 +0700

    Fix perplexity calculation

--- test_perplexity.js
+++ test_perplexity.js
@@ -1,15 +1,28 @@
 var lda = require('./lib/lda');
 var perplexity_lda = require('./lib/perplexity_lda');
 
-var text = 'Cats are small. Dogs are big. Cats like to chase mice. Dogs like to eat bones.';
-var text_test = 'Cats are small. You are small. Tigers are big, Cats chase mice. Dogs eat mice.';
-var documents = text.match( /[^\.!\?]+[\.!\?]+/g );
-var documents_test = text_test.match( /[^\.!\?]+[\.!\?]+/g );
+var sentences = [
+  'คอมพิวเตอร์ เทคโนโลยี่ โลก แสดงผล',
+  'โลก ต้นไม้ ธรรมชาติ ลำธาร เทคโนโลยี่ ทำลาย',
+  'เทคโนโลยี่ โลก ธรรมมะ หลุดพ้น ดับทุกข์',
+  'คอมพิวเตอร์ เทคโนโลยี่ โลก แสดงผล',
+  'โลก ต้นไม้ ธรรมชาติ ลำธาร เทคโนโลยี่ ทำลาย',
+  'เทคโนโลยี่ โลก ธรรมมะ หลุดพ้น ดับทุกข์',
+  'คอมพิวเตอร์ เทคโนโลยี่ โลก แสดงผล',
+  'โลก ต้นไม้ ธรรมชาติ ลำธาร เทคโนโลยี่ ทำลาย',
+  'เทคโนโลยี่ โลก ธรรมมะ หลุดพ้น ดับทุกข์',
+  'คอมพิวเตอร์ เทคโนโลยี่ โลก แสดงผล',
+  'โลก ต้นไม้ ธรรมชาติ ลำธาร เทคโนโลยี่ ทำลาย',
+  'เทคโนโลยี่ โลก ธรรมมะ หลุดพ้น ดับทุกข์',
+  'คอมพิวเตอร์ เทคโนโลยี่ โลก แสดงผล',
+  'โลก ต้นไม้ ธรรมชาติ ลำธาร เทคโนโลยี่ ทำลาย',
+  'เทคโนโลยี่ โลก ธรรมมะ หลุดพ้น ดับทุกข์',
+];
 
-var result = lda('direct', documents, 2, 5, ['th']);
+var result = lda('direct', sentences, 3, 3, ['th']);
 
 console.log(JSON.stringify(result));
 result.printReadableOutput();
 
-var perplexity = perplexity_lda(result.topicModel, documents_test, ['th']);
+var perplexity = perplexity_lda(result.topicModel, sentences, ['th']);
 console.log('Perplexity = '+perplexity);
